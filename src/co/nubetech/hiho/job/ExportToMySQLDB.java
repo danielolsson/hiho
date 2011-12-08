6680fedb9c7177958c8be8025711f55b4b7ff87f
Daniel Olsson <Daniel3.Olsson@sonyericsson.com>
2011-12-08 16:09:30 +0100

    ExportToMySQLDB.java: Reverted changes for last commit

--- src/co/nubetech/hiho/job/ExportToMySQLDB.java
+++ src/co/nubetech/hiho/job/ExportToMySQLDB.java
@@ -1,16 +1,16 @@
 /**
  * Copyright 2010 Nube Technologies
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed
- * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
- * CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and limitations under the License.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed 
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+ * CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and limitations under the License. 
  */
 
 package co.nubetech.hiho.job;
@@ -22,9 +22,8 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.NullWritable;
-import org.apache.hadoop.mapred.JobClient;
-import org.apache.hadoop.mapred.JobConf;
-import org.apache.hadoop.mapred.lib.NullOutputFormat;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.output.NullOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 import org.apache.log4j.Logger;
@@ -33,7 +32,7 @@ import co.nubetech.apache.hadoop.DBConfiguration;
 import co.nubetech.hiho.common.HIHOConf;
 import co.nubetech.hiho.common.HIHOException;
 import co.nubetech.hiho.mapreduce.MySQLLoadDataMapper;
-import co.nubetech.hiho.mapred.input.FileStreamInputFormat;
+import co.nubetech.hiho.mapreduce.lib.input.FileStreamInputFormat;
 
 public class ExportToMySQLDB extends Configured implements Tool {
 
@@ -92,7 +91,7 @@ public class ExportToMySQLDB extends Configured implements Tool {
 			e1.printStackTrace();
 			throw new IOException(e1);
 		}
-		JobConf job = new JobConf(conf, getClass());
+		Job job = new Job(conf);
 		job.setJobName("MySQLBulkLoading");
 		job.setMapperClass(MySQLLoadDataMapper.class);
 		job.setJarByClass(MySQLLoadDataMapper.class);
@@ -106,20 +105,20 @@ public class ExportToMySQLDB extends Configured implements Tool {
 		logger.debug(conf.get(DBConfiguration.PASSWORD_PROPERTY));
 
 		job.setNumReduceTasks(0);
-		job.setInputFormat(FileStreamInputFormat.class);
+		job.setInputFormatClass(FileStreamInputFormat.class);
 		FileStreamInputFormat.addInputPath(job, new Path(inputPath));
 		job.setMapOutputKeyClass(NullWritable.class);
 		job.setMapOutputValueClass(NullWritable.class);
 		// job.setJarByClass(com.mysql.jdbc.Driver.class);
-		job.setOutputFormat(NullOutputFormat.class);
+		job.setOutputFormatClass(NullOutputFormat.class);
 
+		int ret = 0;
 		try {
-			JobClient.runJob(job);
+			ret = job.waitForCompletion(true) ? 0 : 1;
 		} catch (Exception e) {
 			e.printStackTrace();
-			return 1;
 		}
-		return 0;
+		return ret;
 	}
 
 	public static void main(String[] args) throws Exception {
